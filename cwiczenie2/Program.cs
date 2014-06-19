7d2bb425c83e94ed2987578c19801d7ea2851f89
staddaleko <staddaleko@gmail.com>
2014-06-19 18:16:24 +0200

    I have no idea what I'm doing...

---
+++ cwiczenie2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cwiczenie2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double x, y, z, v;
+            Console.Write("Podaj wartosc zmiennej x: ");
+            x = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Podaj wartosc zmiennej y: ");
+            y = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Podaj wartosc zmiennej z: ");
+            z = Convert.ToDouble(Console.ReadLine());
+            v = (4 * x - y) / (3 * (z * z + 1));
+            Console.WriteLine("wartosc wyrazenia wynosi {0}", v);
+        }
+    }
+}
