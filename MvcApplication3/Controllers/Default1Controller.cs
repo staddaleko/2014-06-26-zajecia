ecb83c32aa71de19831e96ac8fbb7aa6429454b1
staddaleko <staddaleko@gmail.com>
2014-06-25 17:19:45 +0200

    wkładamy całą pustą stronę

---
+++ MvcApplication3/Controllers/Default1Controller.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcApplication3.Controllers
+{
+    public class Default1Controller : Controller
+    {
+        //
+        // GET: /Default1/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Kontakt()
+        {
+            return View();
+        }
+
+    }
+}
