beb777bc59270f1db9830b934782bc969aab6bc9
staddaleko <staddaleko@gmail.com>
2014-06-26 18:00:12 +0200

    ostatnie zmiany dzisiaj

--- MvcApplication1/Controllers/GaleriaController.cs
+++ MvcApplication1/Controllers/GaleriaController.cs
@@ -16,7 +16,7 @@ namespace Galeria.Controllers
         {
             return View("Index");
         }
-        public ActionResult Zdjecie(int Id)
+        public ActionResult Zdjecie(int Id=0) //jeżeli nie damy to zera, to będzie błąd serwera przy wyswietlaniu strony
         {
             Zdjecie_c is_z = new Zdjecie_c();
             return View("Zdjecie", is_z);
