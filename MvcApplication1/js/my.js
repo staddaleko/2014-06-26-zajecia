08484a8ac73ab9fba8d44111b4acf8abfc9fb6d3
staddaleko <staddaleko@gmail.com>
2014-06-27 14:12:36 +0200

    pracujemy nad rozwiazaniem, ale efekty mizerne :(

--- MvcApplication1/js/my.js
+++ MvcApplication1/js/my.js
@@ -1,3 +1,11 @@
 $(document).ready(function(){
 Cufon.now();
 });
+
+var title = document.getElementsByTagName("title").toString;
+$("#p1".setAtribute("class",""));
+$("#p2".setAtribute("class",""));
+$("#p3".setAtribute("class",""));
+if(title == "Index") $("#p1").setAtribute("class","active");
+if(title == "Photos") $("#p2").setAtribute("class","active");
+if (title == "Login") $("#p3").setAtribute("class", "active");
